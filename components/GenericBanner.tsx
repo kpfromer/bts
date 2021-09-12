0c7e55d2373cd1cbd5b72639e8386a96d1eadf48
Kyle Pfromer <kpfromer@cu-engr2-1-13-10.201.48.10.int.colorado.edu>
2021-09-12 16:33:46 -0600

    fix: banner

--- components/GenericBanner.tsx
+++ components/GenericBanner.tsx
@@ -7,18 +7,20 @@ export interface GenericBannerProps {
 
 const GenericBanner: React.FC<GenericBannerProps> = ({ img, children }) => {
   return (
-    <div className="relative overflow-hidden rounded mt-3 mb-4">
-      <div className="absolute inset-0 z-0">
-        <Image
-          src={img}
-          alt="Banner background"
-          className="object-cover"
-          layout="fill"
-        />
-      </div>
+    <div>
+      <div className="relative overflow-hidden rounded-lg mt-3 mb-4">
+        <div className="absolute inset-0 z-0">
+          <Image
+            src={img}
+            alt="Banner background"
+            className="object-cover"
+            layout="fill"
+          />
+        </div>
 
-      <div className="z-10 flex flex-col space-y-5 relative backdrop-filter backdrop-blur bg-black bg-opacity-30 py-5 px-3 text-white">
-        {children}
+        <div className="z-10 flex flex-col space-y-5 relative backdrop-filter backdrop-blur bg-black bg-opacity-30 py-5 px-3 text-white">
+          {children}
+        </div>
       </div>
     </div>
   );
