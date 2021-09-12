4ed0627943461fbf71785deb741e772a54c48407
Kyle Pfromer <kpfromer2@gmail.com>
2021-09-12 12:56:23 -0600

    fix: draft 3

--- pages/draft-three.tsx
+++ pages/draft-three.tsx
@@ -1,28 +1,18 @@
-import { DateTime } from "luxon";
 import Head from "next/head";
-import React, { useMemo } from "react";
-import Banner from "../components/Banner";
+import React from "react";
 import { Header } from "../components/Header";
 import ListView from "../components/ListView";
 import Page from "../components/Page";
-import eventsData from "../data/events.json";
+import { getEvents } from "../utils/helpers";
 import useModal from "../utils/use-modal";
 
-export interface DraftThreeProps {}
+const sortedEvents = getEvents();
+
+export interface DraftThreeProps { }
 
 const DraftThree: React.FC<DraftThreeProps> = (props) => {
   const { modal, open, close } = useModal();
 
-  const sortedEvents = useMemo(() => {
-    return eventsData
-      .map((event) => ({
-        data: event,
-        date: DateTime.fromFormat(`${event.date} ${event.startTime}`, "D TT"),
-      }))
-      .filter(({ date }) => date.diffNow().milliseconds > 0)
-      .sort((a, b) => a.date.diff(b.date).milliseconds)
-      .map(({ data }) => data);
-  }, [eventsData]);
 
   return (
     <>
